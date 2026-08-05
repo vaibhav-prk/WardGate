e0ba30f84ea3cb538e24087986d58b2686fb1503
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2026-08-06 00:15:05 +0530

    feat: chain all dependency with server

---
+++ cmd/gateway/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/vaibhav-prk/Wardgate/internal/server"
+)
+
+func main() {
+	// redis connect
+	rdb, err := server.NewRedisClient()
+	if err != nil {
+		log.Fatalf("failed to connect to redis %v", err)
+	}
+
+	log.Println("connected to redis")
+
+	target, err := server.NewBackend()
+	if err != nil {
+		log.Fatalf("invalid backend URL: %v", err)
+	}
+
+	srv := server.NewServer(rdb, target)
+
+	log.Println("Gateway listening on :8080")
+
+	if err := srv.Run(); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+
+	log.Println("Server shut down gracefully")
+}
