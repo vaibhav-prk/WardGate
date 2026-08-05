a8a91a276a5de6588de307454a43d49f5933d234
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2026-08-06 00:14:08 +0530

    feat: add redis depend

---
+++ internal/server/redis.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"context"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient() (*redis.Client, error) {
+	redisURL := os.Getenv("REDIS_URL")
+
+	if redisURL == "" {
+		redisURL = "localhost:6379"
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisURL,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, err
+	}
+
+	return rdb, nil
+}
