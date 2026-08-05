318ce5335a149a6d797ab1e67a6923ff2649a46e
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2026-08-06 00:14:36 +0530

    feat: add mock backend dependency

---
+++ internal/server/mock-backend.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"net/url"
+	"os"
+)
+
+func NewBackend() (*url.URL, error) {
+	backendURL := os.Getenv("BACKEND_URL")
+	if backendURL == "" {
+		backendURL = "http://localhost:8081"
+	}
+
+	target, err := url.Parse(backendURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return target, nil
+}
