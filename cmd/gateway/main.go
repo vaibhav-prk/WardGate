4a56bccb135eaf7ede7c43213a520487f99700c8
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2026-08-06 00:23:34 +0530

    docs: added useful comments

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -9,26 +9,25 @@ import (
 )
 
 func main() {
-	// redis connect
+	// Initialize the Redis client.
 	rdb, err := server.NewRedisClient()
 	if err != nil {
 		log.Fatalf("failed to connect to redis %v", err)
 	}
-
 	log.Println("connected to redis")
 
+	// Resolve the backend service URL.
 	target, err := server.NewBackend()
 	if err != nil {
 		log.Fatalf("invalid backend URL: %v", err)
 	}
 
+	// Construct and run the API gateway server.
 	srv := server.NewServer(rdb, target)
-
 	log.Println("Gateway listening on :8080")
 
 	if err := srv.Run(); err != nil {
 		log.Fatalf("server error: %v", err)
 	}
-
 	log.Println("Server shut down gracefully")
 }
